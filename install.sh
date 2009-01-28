66496ae683a5008cc0ce7ba318d48badaceb47a0
Kenneth Miller <ken@siren.local>
2009-01-28 15:54:27 -0800

    adding an installer

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+ln -shFi ~/.config/bash/profile ~/.bash_profile
+ln -shFi ~/.config/vim ~/.vim
+ln -shFi ~/.config/vim/vimrc ~/.vimrc
+ln -shFi ~/.config/vim/gvimrc ~/.gvimrc
+
+
