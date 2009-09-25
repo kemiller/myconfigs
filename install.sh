ee23c2097706e94ab06090b9b7b253fe5c68c026
Ken Miller <ken@corp.mamasource.com>
2009-09-25 10:19:11 -0700

    making install script linux-compatible

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-ln -shFi ~/.config/bash/profile ~/.bash_profile
-ln -shFi ~/.config/vim ~/.vim
-ln -shFi ~/.config/vim/vimrc ~/.vimrc
-ln -shFi ~/.config/vim/gvimrc ~/.gvimrc
-ln -shFi ~/.config/bin ~/bin
-ln -shFi ~/.config/gitconfig ~/.gitconfig
+ln -snFi ~/.config/bash/profile ~/.bash_profile
+ln -snFi ~/.config/vim ~/.vim
+ln -snFi ~/.config/vim/vimrc ~/.vimrc
+ln -snFi ~/.config/vim/gvimrc ~/.gvimrc
+ln -snFi ~/.config/bin ~/bin
+ln -snFi ~/.config/gitconfig ~/.gitconfig
 
 
