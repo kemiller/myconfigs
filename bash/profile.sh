7620d3a8fe42fa581ba8d03c546e775f1caf511e
Ken Miller <ken@bitfield.net>
2012-04-19 22:21:08 -0700

    supporting rbenv in rails.sh

--- bash/profile.sh
+++ bash/profile.sh
@@ -53,8 +53,10 @@ complete -o bashdefault -o default -o nospace -F _git g
 ## Prompt
 ##
 
-function my-rvm-prompt() {
-  if which rvm-prompt > /dev/null 2>&1; then
+function ruby-version-prompt() {
+  if which rbenv-prompt > /dev/null 2>&1; then
+    echo " $(rbenv-prompt $1)"
+  elif which rvm-prompt > /dev/null 2>&1; then
     echo " $(rvm-prompt $1)"
   fi
 }
@@ -69,7 +71,7 @@ export GIT_PS1_SHOWDIRTYSTATE=true
 #export GIT_PS1_DESCRIBE_STYLE=branch
 #export GIT_PS1_SHOWUNTRACKEDFILES=true
 
-export PS1='$(detect_rails_dir)$(gitprompt=`my-git-prompt`; echo "\[\e]0;\w $gitprompt\007\]\n\[\e[${PROMPTCOLOR};1;7m\]\h \w \[\e[0m\] \t${gitprompt}$(my-rvm-prompt u)\n➔ ")'
+export PS1='$(detect_rails_dir)$(gitprompt=`my-git-prompt`; echo "\[\e]0;\w $gitprompt\007\]\n\[\e[${PROMPTCOLOR};1;7m\]\h \w \[\e[0m\] \t${gitprompt}$(ruby-version-prompt u)\n➔ ")'
 
 ## 
 ## Misc
@@ -94,4 +96,13 @@ function shave_and_a_haircut() {
 	
 if [[ -d $HOME/.ec2 ]] ; then source $HOME/.ec2/vars; fi
 if [[ -s $HOME/.rvm/scripts/rvm ]] ; then source $HOME/.rvm/scripts/rvm ; fi
+
+
+##
+# Your previous /Users/ken/.bash_profile file was backed up as /Users/ken/.bash_profile.macports-saved_2012-03-04_at_01:15:03
+##
+
+# MacPorts Installer addition on 2012-03-04_at_01:15:03: adding an appropriate PATH variable for use with MacPorts.
+export PATH=/opt/local/bin:/opt/local/sbin:$PATH
+# Finished adapting your PATH environment variable for use with MacPorts.
 
