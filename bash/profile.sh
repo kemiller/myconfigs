46cb552259df8f7d6b0c16244c65f2ef98802333
Ken Miller <ken@bitfield.net>
2012-12-10 10:47:59 -0800

    removing unauthorized additions

--- bash/profile.sh
+++ bash/profile.sh
@@ -99,12 +99,3 @@ function shave_and_a_haircut() {
 if [[ -d $HOME/.ec2 ]] ; then source $HOME/.ec2/vars; fi
 if [[ -s $HOME/.rvm/scripts/rvm ]] ; then source $HOME/.rvm/scripts/rvm ; fi
 
-
-##
-# Your previous /Users/ken/.bash_profile file was backed up as /Users/ken/.bash_profile.macports-saved_2012-03-04_at_01:15:03
-##
-
-# MacPorts Installer addition on 2012-03-04_at_01:15:03: adding an appropriate PATH variable for use with MacPorts.
-export PATH=/opt/local/bin:/opt/local/sbin:$PATH
-# Finished adapting your PATH environment variable for use with MacPorts.
-
