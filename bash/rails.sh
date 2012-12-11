bc8261ac07d442862ba4630b7db1a477dbb7a8b9
Ken Miller <ken@bitfield.net>
2012-12-10 20:52:47 -0800

    adding alias and cleaning up

--- bash/rails.sh
+++ bash/rails.sh
@@ -1,22 +1,11 @@
 #!/bin/bash
 
-#
-# Defaults
-#
-
 DIR_ABBREVS="ac:app/controllers am:app/models aa:app/admin ah:app/helpers"
 DIR_ABBREVS="$DIR_ABBREVS tf:test/functional tu:test/unit ti:test/integration"
 DIR_ABBREVS="$DIR_ABBREVS db:db mg:db/migrate tc:test/factories lb:lib av:app/views"
 DIR_ABBREVS="$DIR_ABBREVS aw:app/workers"
 
 SCRIPT_ABBREVS="ss:server cn:console dbc:dbconsole gen:generate"
-
-# Rails Directory Navigation
-
-
-alias rake="rsr bundle rake"
-alias rails="rsr bundle rails"
-alias vagrant="rsr bundle vagrant"
 
 for pair in $SCRIPT_ABBREVS; do
   shortcut=$(echo $pair | cut -d: -f1)
@@ -30,18 +19,19 @@ for pair in $DIR_ABBREVS; do
   eval "$(rsr alias $shortcut $longcut)"
 done
 
+alias a='cd $(rsr path)'
+alias rake="rsr bundle rake"
+alias rails="rsr bundle rails"
+alias vagrant="rsr bundle vagrant"
 alias ts="rsr test"
-complete -C "rsr testlist" ts
-
-# Misc
+alias rgrep="grep -r --include='*.rb'"
+alias rack="ack --ruby"
 
 function gd () {
   pushd $RUBYROOT/gems/1.8/gems/$1
 }
+
 complete -W '`\ls -1 $RUBYROOT/gems/1.8/gems`' gd
-
-alias rgrep="grep -r --include='*.rb'"
-alias rack="ack --ruby"
-
+complete -C "rsr testlist" ts
 complete -C ~/bin/rake_bash_complete -o default rake
 
